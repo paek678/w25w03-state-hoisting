fccee3635f7ca9771f135c6893e931650c51b04d
baek seoung youl <paek678@naver.com>
2025-09-25 13:35:41 +0900

    💡State hoisting

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,34 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+    const [count1, setCount1] = useState(0)
+    const [count2, setCount2] = useState(0)
+
   return (
     <div>
-      <Counter/>
-      <Counter/>
+      <Counter
+      count = {count1}
+      onLIncrement = {() => setCount1(prev => prev + 1) }/>
+      
+      <Counter
+      count = {count2}
+      onLIncrement = {() => setCount2(prev => prev + 1) }/>
 
     </div>
   )
 }
 
-function Counter() {
-  const [count, setCount] = useState(0)
-
+function Counter({count , onLIncrement}) {
+  //const [count, setCount] = u  
   return (
     <div>
       <h1>Counter: {count}</h1>
-      <button 
-        onClick={ () => setCount( prev => prev + 1 ) }>
-          증가
+      <button onClick={onIncrement}>
+        증가
       </button>
     </div>
   )
+
 }
 
 export default App
